ee7ea319963d309595ead961203713e640e6e336
git6666666 <93507581+git6666666@users.noreply.github.com>
2026-01-14 22:56:08 +0800

    更新 cpus.js

--- src/data/cpus.js
+++ src/data/cpus.js
@@ -6,9 +6,10 @@ export const cpus = [
   // AMD
   { name: "Ryzen 9 7950X3D", brand: "amd", releaseDate: "2023-02-28", rps: 410 },
   { name: "Ryzen 7 7800X3D", brand: "amd", releaseDate: "2023-04-06", rps: 365 },
-  { name: "Ryzen 7 8845hs", brand: "amd", releaseDate: "2023-04-06", rps: 365 },
+  { name: "Ryzen 7 8845hs", brand: "amd", releaseDate: "2023-12-06", rps: 278 },
 
   // Apple
   { name: "M3 Max", brand: "apple", releaseDate: "2023-10-30", rps: 300 },
   { name: "M2 Ultra", brand: "apple", releaseDate: "2023-06-05", rps: 275 },
+  { name: "M4", brand: "apple", releaseDate: "2024-05-30", rps: 329 },
 ];
